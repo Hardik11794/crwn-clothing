65a8c986b18de3142eedfdbb6ee45b93ebd6371c
Hardik <hardik11794@gmail.com>
2021-04-28 13:09:03 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer'
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const composedEnhancer = compose(applyMiddleware(...middlewares),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const store = createStore(rootReducer,composedEnhancer )
 
 export default store;
