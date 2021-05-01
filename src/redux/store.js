1b5a8defc81fafe1de30bfee664d18c355775469
Hardik <hardik11794@gmail.com>
2021-05-01 17:58:31 -0400

    modified devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,16 +8,11 @@ const middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
-    const composedEnhancer = compose(applyMiddleware(...middlewares),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-} else {
-    const composedEnhancer = compose(applyMiddleware(...middlewares))
-}
-
     
+} 
+  
+// const composedEnhancer = compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-
+const composedEnhancer = compose(applyMiddleware(...middlewares))
 export const store = createStore(rootReducer,composedEnhancer )
-
 export const persistor = persistStore(store);
-
-// export default {store, persistor};
