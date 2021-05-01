a38c5d2d311ee17e9654726a3ec7ea8ec0e3ca5f
Hardik <hardik11794@gmail.com>
2021-05-01 17:14:11 -0400

    remove reduxdevtool from production env and simplified the code

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,15 +8,12 @@ const middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
-}
-
-if (process.env.NODE_ENV === 'production') {
-    const composedEnhancer = compose(applyMiddleware(...middlewares))
+    const composedEnhancer = compose(applyMiddleware(...middlewares),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 } else {
-    const composedEnhancer = compose(applyMiddleware(...middlewares),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())    
+    const composedEnhancer = compose(applyMiddleware(...middlewares))
 }
-    
 
+    
 
 
 export const store = createStore(rootReducer,composedEnhancer )
